21e2a3336d65a4773f57cb4c95acc59643a14960
Troy <troyowns@hotmail.co.uk>
2020-03-20 15:29:38 +0000

    ejs partials for header and footer

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use("/", require("./routes/index.js"));
 app.use("/users", require("./routes/users.js"));
