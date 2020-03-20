dc9ad395673f8b1c201238f6a68ea7cdc2ed9f1a
Troy <troyowns@hotmail.co.uk>
2020-03-20 19:55:41 +0000

    added warnings to sign up

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/register",  function(req, res) {
+router.get("/register", function(req, res) {
   res.render("register");
 });
 
 
-router.get("/login",  function(req, res) {
+router.get("/login", function(req, res) {
   res.render("login");
 });
 
 router.post("/register", function(req, res) {
-  console.log(req.body);
-  res.send("welcome");
+  const username = req.body.name;
+  const password = req.body.password;
+  var errors = [];
+  if (!username || !password) {
+    errors.push({
+      message: "please fill in fields"
+    });
+  }
+  if (password.length < 6) {
+    errors.push({
+      message: "passwords need to be at least 6 chars"
+    });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      error: errors,
+    });
+  } else {
+    res.send("pass");
+  }
 });
 
 module.exports = router;
