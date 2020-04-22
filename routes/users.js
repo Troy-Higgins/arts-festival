32537b84a9d52000c94c455a1cb304a0515c6111
Troy <troyowns@hotmail.co.uk>
2020-04-22 13:34:21 +0100

    tickets can now be deleted from the account page

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,8 @@ const passport = require("passport");
 const auth = require("../config/auth");
 const isAuthenticated = auth.ensureAuthenticated;
 const isNotAuthenticated = auth.isNotAuthenticated;
-const genOrder = require("../custom-module/generateOrder");
-const generateOrder = genOrder.reserveTicket;
+const orders = require("../custom-module/orderFunctions");
+const generateOrder = orders.reserveTicket;
 const renderPage = require("../custom-module/renderPage");
 const bodyParser = require("body-parser");
 const email = require("../config/nodemailer");
@@ -34,108 +34,114 @@ router.get("/tickets", isAuthenticated, function(req, res) {
   renderPage.renderTicketPage(req, res, userEmail);
 });
 
-router.post("/ticketOrder", isAuthenticated, function(req, res) {
-  //req.flash("successMessage", "you have logged out");
-  //console.log(req.user);
-  const userEmail = req.user.email;
-  const ticketNumber = req.body.NoOfTickets;
-  const successMessage = req.body.NoOfTickets + " tickets reserved";
-  generateOrder(req, res, userEmail, ticketNumber);
-  //req.flash("successMessage", successMessage);
-  //res.redirect("/users/account");
-});
 
-
-router.get("/account", isAuthenticated, function(req, res) {
-  //email.testEmail();
-  const userEmail = req.user.email;
-  renderPage.renderAccountPage(req, res, userEmail);
-});
-
-router.post("/logout", function(req, res) {
-  req.logout();
-  req.flash("successMessage", "you have logged out");
-  res.redirect("/users/login");
-});
-
-router.post("/register", function(req, res) {
-  var errors = [];
-  const username = req.body.email;
-  const password = req.body.password;
-  const password2 = req.body.password2;
-  if (!username || !password) {
-    errors.push({
-      message: "please fill in fields"
+router.post("/deleteOrder", isAuthenticated, function(req, res) {
+      const userEmail = req.user.email;
+      orders.deleteOrder(req, res, userEmail)
     });
-  }
-  if (password.length < 6) {
-    errors.push({
-      message: "passwords need to be at least 6 chars"
-    });
-    console.log("passwords need to be at least 6 chars");
-  }
-  if (password != password2) {
-    errors.push({
-      message: "passwords do not match"
-    });
-  }
-  if (errors.length > 0) {
-    res.render("sign-up", {
-      errors: errors,
-    });
-  } else {
-    //Form details appear valid!
-    User.findOne({
-        email: username
-      },
-      function(err, foundUser) {
-        //check if a user already exists
-        if (foundUser) {
-          //if they already exist
-          console.log("user found");
-          errors.push({
-            message: "user already registered"
-          });
-          res.render("sign-up", {
-            errors: errors,
-          });
-        } else {
-          //success, lets make a new data model for them
 
-          bcrypt.hash(password, saltRounds, function(err, hash) {
-            if (!err) {
-              const newUser = new User({
-                email: username,
-                password: hash
+    router.post("/ticketOrder", isAuthenticated, function(req, res) {
+      //req.flash("successMessage", "you have logged out");
+      //console.log(req.user);
+      const userEmail = req.user.email;
+      const ticketNumber = req.body.NoOfTickets;
+      const successMessage = req.body.NoOfTickets + " tickets reserved";
+      generateOrder(req, res, userEmail, ticketNumber);
+      //req.flash("successMessage", successMessage);
+      //res.redirect("/users/account");
+    });
+
+
+    router.get("/account", isAuthenticated, function(req, res) {
+      //email.testEmail();
+      const userEmail = req.user.email;
+      renderPage.renderAccountPage(req, res, userEmail);
+    });
+
+    router.post("/logout", function(req, res) {
+      req.logout();
+      req.flash("successMessage", "you have logged out");
+      res.redirect("/users/login");
+    });
+
+    router.post("/register", function(req, res) {
+      var errors = [];
+      const username = req.body.email;
+      const password = req.body.password;
+      const password2 = req.body.password2;
+      if (!username || !password) {
+        errors.push({
+          message: "please fill in fields"
+        });
+      }
+      if (password.length < 6) {
+        errors.push({
+          message: "passwords need to be at least 6 chars"
+        });
+        console.log("passwords need to be at least 6 chars");
+      }
+      if (password != password2) {
+        errors.push({
+          message: "passwords do not match"
+        });
+      }
+      if (errors.length > 0) {
+        res.render("sign-up", {
+          errors: errors,
+        });
+      } else {
+        //Form details appear valid!
+        User.findOne({
+            email: username
+          },
+          function(err, foundUser) {
+            //check if a user already exists
+            if (foundUser) {
+              //if they already exist
+              console.log("user found");
+              errors.push({
+                message: "user already registered"
               });
-              console.log(newUser);
-              //save the user to users collection
-              newUser.save(function(err) {
-                if (!err) {
-                  email.signupEmail(username);
-                  req.flash('successMessage', 'You now have an account');
-                  res.redirect("/users/login");
-                } else {
-                  console.log("save error");
-                }
+              res.render("sign-up", {
+                errors: errors,
               });
             } else {
-              //if hash fails then log the result
-              console.log(err);
+              //success, lets make a new data model for them
+
+              bcrypt.hash(password, saltRounds, function(err, hash) {
+                if (!err) {
+                  const newUser = new User({
+                    email: username,
+                    password: hash
+                  });
+                  console.log(newUser);
+                  //save the user to users collection
+                  newUser.save(function(err) {
+                    if (!err) {
+                      email.signupEmail(username);
+                      req.flash('successMessage', 'You now have an account');
+                      res.redirect("/users/login");
+                    } else {
+                      console.log("save error");
+                    }
+                  });
+                } else {
+                  //if hash fails then log the result
+                  console.log(err);
+                }
+              });
             }
           });
-        }
-      });
-  }
-});
+      }
+    });
 
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', {
-    successRedirect: '/users/account',
-    failureRedirect: '/users/login',
-    failureFlash: true
-  })(req, res, next)
-});
+    router.post('/login', function(req, res, next) {
+      passport.authenticate('local', {
+        successRedirect: '/users/account',
+        failureRedirect: '/users/login',
+        failureFlash: true
+      })(req, res, next)
+    });
 
 
-module.exports = router;
+    module.exports = router;
