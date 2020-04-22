594c740783a62c19adf70b6e64c46b87bedd713f
Troy <troyowns@hotmail.co.uk>
2020-04-22 13:45:28 +0100

    test commit on master

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+//test commit
 //const passportLocalMongoose = require('passport-local-mongoose');
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
